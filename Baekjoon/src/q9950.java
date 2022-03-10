641ef0166df83463c783843bdc55a9b98f046f4e
rae-eun <didfodms@gmail.com>
2022-03-10 19:32:16 +0900

    solved 9950

--- Baekjoon/src/q9950.java
+++ Baekjoon/src/q9950.java
@@ -1,13 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class q9950 {
 
 	public static void main(String[] args) {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(a==0 && b==0 && c==0) break;
+			
+			String res = "";
 
+			if(a!=0) {
+				res = a+" ";
+			} else {
+				res = c/b+" ";
+			}
+			
+			if(b!=0) {
+				res += b+" ";
+			} else {
+				res += c/a+" ";
+			}
+			
+			if(c!=0) {
+				res += c;
+			} else {
+				res += a*b;
+			}
+			
+			System.out.println(res);
+
+		}
+		sc.close();
 	}
 
 }
